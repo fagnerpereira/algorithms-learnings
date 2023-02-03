7e6af26fb3a2bb875e04b737e010884d815d5fc3
Fagner Rosa <fagnerfpr@gmail.com>
2023-02-03 19:13:46 -0300

    update tournament winner algorithm

--- tournament_winner.rb
+++ tournament_winner.rb
@@ -1,21 +1,32 @@
 module TournamentWinner
+  SCORES = Hash.new { |k, v| k[v] = 0 }
+
   def self.call(competitions, results)
-    # [
-    #   ["HTML", "C#"],
-    #   ["C#", "Python"],
-    #   ["Python", "HTML"]
-    # ],
-    # [0, 0, 1]
-    # O(log(n))
-    wins = Hash.new { |k, v| k[v] = 0 }
+    # O(n)
+    current_winner = ''
+    leader = ''
     competitions.each_with_index do |competition, i|
       home, away = competition
       if results[i] == 0
-        wins[away] += 3
+        SCORES[away] += 3
+        current_winner = away
       elsif results[i] == 1
-        wins[home] += 3
+        SCORES[home] += 3
+        current_winner = home
       end
+      leader = update_leadership!(current_winner, leader)
     end
-    wins.sort_by { |k, v| v }.last.first
+
+    leader
+  end
+
+  def self.update_leadership!(current_winner, leader)
+    return current_winner if leader.empty?
+
+    if SCORES[current_winner].to_i > SCORES[leader].to_i
+      current_winner
+    else
+      leader
+    end
   end
 end
